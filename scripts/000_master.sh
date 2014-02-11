db8d49bb0f710ae186ab8e5a1699de34b8c2a7f1
Nate Clark <nclark@knotice.com>
2014-02-11 12:28:59 -0500

    Script changes

--- scripts/000_master.sh
+++ scripts/000_master.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo wget "https://raw2.github.com/n8io/raspbmc-pvr/master/scripts/001_update_pi.sh" -v -O "./scripts/001_update_pi.sh" --no-check-certificate
-chmod a+x ./scripts/*
+sudo chmod a+x ./scripts/*
 sudo ./scripts/001_update_pi.sh
